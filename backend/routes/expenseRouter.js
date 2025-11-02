c02fcb9170d3c8e50d813af37428b78cecb8f574
maniac-raj <dharanraj1010@gmail.com>
2025-11-02 18:50:20 +0530

    upgraded readability of router

--- backend/routes/expenseRouter.js
+++ backend/routes/expenseRouter.js
@@ -2,9 +2,9 @@ const express = require('express');
 const { createExpense, getExpense, updateExpense, deleteExpense } = require('../cotrollers/expenseController');
 const expenseRouter = express.Router();
 
-expenseRouter.post('/create-expense', createExpense);
-expenseRouter.get('/get-expenses', getExpense);
-expenseRouter.put('/update-expense/:id', updateExpense);
-expenseRouter.delete('/delete-expense/:id', deleteExpense);
+expenseRouter.post('/create-expense', createExpense)
+ .get('/get-expenses', getExpense)
+ .put('/update-expense/:id', updateExpense)
+ .delete('/delete-expense/:id', deleteExpense)
 
 module.exports = { expenseRouter }
